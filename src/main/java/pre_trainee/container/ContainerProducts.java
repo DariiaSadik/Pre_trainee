41ac92320d56fb663a5019a7fc12b1066e815e7c
DariiaSadik <dasha.sadik@gmail.com>
2015-07-22 19:08:36 +0300

    22/07/2015

--- src/main/java/pre_trainee/container/ContainerProducts.java
+++ src/main/java/pre_trainee/container/ContainerProducts.java
@@ -8,7 +8,7 @@ import pre_trainee.products.ComputerDescription;
 import java.util.*;
 
 public class ContainerProducts {
-    private List<AbstractProductDescription> store =  new ArrayList<AbstractProductDescription>();
+    private List<AbstractProductDescription> store = new ArrayList<AbstractProductDescription>();
 
     public void addNewProducts(AbstractProductDescription abstractProductDescription) {
         store.add(abstractProductDescription);
@@ -35,55 +35,56 @@ public class ContainerProducts {
         return "--------------------------- Container --------------------------" + information(store);
     }
 
-    public String information(List<AbstractProductDescription> store ) {
+    public String information(List<AbstractProductDescription> store) {
         String information = "";
-        for(AbstractProductDescription elem : store){
+        for (AbstractProductDescription elem : store) {
             information += (elem + "  ");
         }
         return information;
     }
-    public static void main(String[] args) {
-        ContainerProducts containerProducts = new ContainerProducts();
-        BookDescription bookDescription = new BookDescription("1", "$9", "1", "1", 111);
-        BookDescription bookDescription2 = new BookDescription("1", "$8", "1", "1", 111);
-        BookDescription bookDescription3 = new BookDescription("1", "$6", "1", "1", 111);
-        BookDescription bookDescription4 = new BookDescription("1", "$3", "1", "1", 111);
-        BookDescription bookDescription5 = new BookDescription("1", "$1", "1", "1", 111);
-
-        Map<String, String> map1 = new HashMap<String, String>();
-        map1.put("11", "1");
-        map1.put("21", "1");
-        map1.put("13", "1");
-        map1.put("41", "1");
-        map1.put("15", "1");
-
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("1", "1");
-        map.put("2", "1");
-        map.put("3", "1");
-        map.put("4", "1");
-        map.put("5", "1");
-
-        ComputerDescription computerDescription = new ComputerDescription("", "$3", map);
-        ComputerDescription computerDescription1 = new ComputerDescription("", "$2", map1);
-
-        containerProducts.addNewProducts(bookDescription);
-        containerProducts.addNewProducts(bookDescription2);
-        containerProducts.addNewProducts(bookDescription3);
-        containerProducts.addNewProducts(bookDescription4);
-        containerProducts.addNewProducts(bookDescription5);
-        containerProducts.addNewProducts(computerDescription1);
-        containerProducts.addNewProducts(computerDescription);
-
-
-        System.out.println(containerProducts);
-        System.out.println("-------------------------------------------------------------------");
-        containerProducts.sortedStore();
-        System.out.println(containerProducts);
-
-
-        System.out.println("-------------------------------------------------------------------");
-        System.out.println(containerProducts.count());
-        System.out.println(containerProducts.getProduct(5));
-    }
 }
+//    public static void main(String[] args) {
+//        ContainerProducts containerProducts = new ContainerProducts();
+//        BookDescription bookDescription = new BookDescription("1", "$9", "1", "1", 111);
+//        BookDescription bookDescription2 = new BookDescription("1", "$8", "1", "1", 111);
+//        BookDescription bookDescription3 = new BookDescription("1", "$6", "1", "1", 111);
+//        BookDescription bookDescription4 = new BookDescription("1", "$3", "1", "1", 111);
+//        BookDescription bookDescription5 = new BookDescription("1", "$1", "1", "1", 111);
+//
+//        Map<String, String> map1 = new HashMap<String, String>();
+//        map1.put("11", "1");
+//        map1.put("21", "1");
+//        map1.put("13", "1");
+//        map1.put("41", "1");
+//        map1.put("15", "1");
+//
+//        Map<String, String> map = new HashMap<String, String>();
+//        map.put("1", "1");
+//        map.put("2", "1");
+//        map.put("3", "1");
+//        map.put("4", "1");
+//        map.put("5", "1");
+//
+//        ComputerDescription computerDescription = new ComputerDescription("", "$3", map);
+//        ComputerDescription computerDescription1 = new ComputerDescription("", "$2", map1);
+//
+//        containerProducts.addNewProducts(bookDescription);
+//        containerProducts.addNewProducts(bookDescription2);
+//        containerProducts.addNewProducts(bookDescription3);
+//        containerProducts.addNewProducts(bookDescription4);
+//        containerProducts.addNewProducts(bookDescription5);
+//        containerProducts.addNewProducts(computerDescription1);
+//        containerProducts.addNewProducts(computerDescription);
+//
+//
+//        System.out.println(containerProducts);
+//        System.out.println("-------------------------------------------------------------------");
+//        containerProducts.sortedStore();
+//        System.out.println(containerProducts);
+//
+//
+//        System.out.println("-------------------------------------------------------------------");
+//        System.out.println(containerProducts.count());
+//        System.out.println(containerProducts.getProduct(5));
+//    }
+//}
